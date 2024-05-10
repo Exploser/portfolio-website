96edce0f822094ed0a6d52b7832dcc0f3e29afd3
Exploser <exploserbusiness@gmail.com>
2024-05-09 20:06:44 -0400

    Add styling and animations to the banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -49,7 +49,9 @@ export const Banner = () => {
                         <span className="tagline">Welcome to my Portfolio!</span>
                         <h1>{`Hi! I'm Abhijeet `}<span className="wrap">{text}</span></h1>
                         <p>Sample text to test my typing speed and to also fill out this section of the page</p>
-                        <button onClick={() => console.log('Connect')}>Connect!<ArrowRightCircle size={25} /> </button>
+                        <div id="button-box">
+                            <button onClick={() => console.log('Connect')}>Connect!<ArrowRightCircle size={25} /> </button>
+                        </div>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Headder Image"/>
