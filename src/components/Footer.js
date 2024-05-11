1753aabbf921bcba42b2a6bd20c534856e5584cb
Exploser <exploserbusiness@gmail.com>
2024-05-11 14:39:29 -0400

    Fix footer social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import { Col, Container, Row } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import linkedinIcon from '../assets/img/nav-icon1.svg'
+import githubIcon from '../assets/img/nav-icon2.svg'
+import thangsIcon from '../assets/img/nav-icon3.svg'
 
 export const Footer = () => {
     return(
@@ -10,13 +10,13 @@ export const Footer = () => {
             <Container>
                 <Row className="align-item-center">
                     <Col sm={6}>
-                        <img src={logo} alt="logo"></img>
+                        <img src={logo} alt="logo" id="footer-logo"></img>
                     </Col>
                     <Col sm={6} className="text-center text-sm-end">
-                        <div className="social-icon">
-                            <a href=""><img src={navIcon1} /></a>
-                            <a href=""><img src={navIcon2} /></a>
-                            <a href=""><img src={navIcon3} /></a>
+                    <div className="social-icon">
+                            <a href="https://www.linkedin.com/in/abhijeetsthakur/"><img src={linkedinIcon} alt="" /></a>
+                            <a href="https://github.com/Exploser"><img src={githubIcon} alt="" /></a>
+                            <a href="https://thangs.com/designer/Expl0ser"><img src={thangsIcon} id="thangs-logo" alt="" /></a>
                         </div>
                         <p>Footer Text. It's me</p>
                     </Col>
