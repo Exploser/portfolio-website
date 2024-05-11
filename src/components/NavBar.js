0c6bc645fbfa7c269f160aca3da282898155513e
Exploser <exploserbusiness@gmail.com>
2024-05-11 14:25:34 -0400

    Fix links in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/img/logo.svg'
-import navIcon1 from '../assets/img/nav-icon1.svg'
-import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
+import linkedinIcon from '../assets/img/nav-icon1.svg'
+import githubIcon from '../assets/img/nav-icon2.svg'
+import thangsIcon from '../assets/img/nav-icon3.svg'
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -46,9 +46,9 @@ export const NavBar = () => {
                     </Nav>
                     <span className="navbar-text">
                         <div className="social-icon">
-                            <a href="#"><img src={navIcon1} alt="" /></a>
-                            <a href="#"><img src={navIcon2} alt="" /></a>
-                            <a href="#"><img src={navIcon3} alt="" /></a>
+                            <a href="https://www.linkedin.com/in/abhijeetsthakur/"><img src={linkedinIcon} alt="" /></a>
+                            <a href="https://github.com/Exploser"><img src={githubIcon} alt="" /></a>
+                            <a href="https://thangs.com/designer/Expl0ser"><img src={thangsIcon} id="thangs-logo" alt="" /></a>
                         </div>
                     </span>
                 </Navbar.Collapse>
